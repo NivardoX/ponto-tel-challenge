1367293376860893dee387cce9972719ab1457ea
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-12-17 00:08:45 -0300

    (dev): implement cache using redis

--- api/app.py
+++ api/app.py
@@ -1,9 +1,12 @@
+import os
+import redis
 import crawler
 import validators
 from quart_openapi import Pint, Resource
 from quart import request, jsonify
 from marshmallow import Schema, fields
 app = Pint(__name__, title='Sample App')
+rclient = redis.from_url(os.environ.get("REDIS_URL"))
 
 
 class CountSchema(Schema):
@@ -48,8 +51,30 @@ class Root(Resource):
                 errors.update({'success': False})
                 return jsonify(errors)
 
+        # cached and to crawler urls
+        cached = {}
+        nocached = []
+        for url in result['urls']:
+            cache = rclient.hget(result['word'], url)
+            if cache is None:
+                nocached.append(url)
+            else:
+                cached.update({url: cache.decode()})
+
         # with all params are ok, than count
-        count = await crawler.count(result['word'], result['urls'])
+        count = {}
+        if nocached != []:
+            count = await crawler.count(result['word'], nocached)
+
+        # cache crawled pages
+        for url in count.keys():
+            rclient.hset(result['word'], url, count[url])
+
+        # results
+        urls = {}
+        urls.update(count)
+        urls.update(cached)
+
         resp = {'success': True}
-        resp.update({"urls": [count]})
+        resp.update({"urls": urls})
         return jsonify(resp)
